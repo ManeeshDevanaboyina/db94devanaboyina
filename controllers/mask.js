be5f5e584444f4d6e596cb0f689a135206572977
ManeeshDevanaboyina <32562977+ManeeshDevanaboyina@users.noreply.github.com>
2021-11-11 00:30:32 -0600

    Updated

--- controllers/mask.js
+++ controllers/mask.js
@@ -1,8 +1,19 @@
 var mask = require('../models/mask');
-// List of all masks
-exports.mask_list = function(req, res) {
-res.send('NOT IMPLEMENTED: mask list');
+
+
+// List of all mask
+exports.mask_list = async function (req, res) {
+    try {
+        themask = await mask.find();
+        res.send(themask);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
+
+
 // for a specific mask.
 exports.mask_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: mask detail: ' + req.params.id);
